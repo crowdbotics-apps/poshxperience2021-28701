06a1ed9255ecc839700ac460c71512412bdba507
Crowdbotics <team@crowdbotics.com>
2021-07-10 23:40:45 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/poshxperience2021_28701/MainActivity.java
@@ -0,0 +1,15 @@
+package com.poshxperience2021_28701;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "poshxperience2021_28701";
+  }
+}
